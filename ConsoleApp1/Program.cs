e61e7a0f71c7771f6f7efd2eb5c4cd1f97a99f9e
Oleg Filimonov <ofilimonov@langate.com>
2019-09-05 16:33:32 +0300

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(IPAddress.Loopback.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // Setup endpoint of server, bind soket to endpoint, and start listen.
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Loopback, 11000);
+                socket.Bind(ipEndPoint);
+                socket.Listen(10);
+
+                // Start get data form clients
+                while (true)
+                {
+                    Console.WriteLine("Wait connection form: {0}", ipEndPoint);
+                    Socket handler = socket.Accept();
+
+                    // recive data from client to buffer
+                    StringBuilder sb = new StringBuilder(1024);
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = 0;
+                    do
+                    {
+                        bytesRec = handler.Receive(bytes);
+                        sb.Append(Encoding.UTF8.GetString(bytes, 0, bytesRec));
+                    }
+                    while (bytesRec > 0 && bytesRec >= bytes.Length);
+
+                    string data = sb.ToString();
+                    Console.Write("Recived Message: " + data + "\n");
+
+                    // Send reply|echo
+                    string reply = "You send message of " + data.Length.ToString() + " symbols";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    try
+                    {
+                        if (data.IndexOf("<cmd_end>") > -1)
+                        {
+                            Console.WriteLine("Connection is closed.");
+                            break;
+                        }
+                    }
+                    finally
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SocketClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SendMessageFromSocket(11000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void SendMessageFromSocket(int port)
+        {
+            // Create remote endpoint and socket for remote connection
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Loopback, port);
+            Socket sender = new Socket(IPAddress.Loopback.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(ipEndPoint);
+            Console.WriteLine("Socked was connect to {0} ", sender.RemoteEndPoint.ToString());
+
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+            int bytesSent = sender.Send(msg); // send data
+
+            // Recive responce
+            byte[] bytes = new byte[1024];
+            int bytesRec = sender.Receive(bytes);
+
+            Console.WriteLine("\nServer responce: {0}\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+            if (message.IndexOf("<cmd_end>") == -1)
+                SendMessageFromSocket(port);
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
